b9378f11b4b315f16251d6c8c65f65a49f089aad
anjanasainju <anjanasainju@gmail.com>
2021-09-29 21:13:46 -0400

    About us page

--- App.js
+++ App.js
@@ -37,8 +37,20 @@ function HomeScreen({ navigation }) {
 function AboutScreen ({navigation}){
   // Gives information about our mission
   return(
-    <View>
-
+    <View style={styles.container}>
+      <Text style ={styles.header}>Vision Statement</Text>
+      <Text style= {styles.paragraphs}> Knight Bites serves Calvin University students who, after the pandemic, wish to create meaningful connections with other Calvin University students and become more comfortable with the Grand Rapids community. It is a meet-up app that connects students through shared meals at restaurants that Calvin University is affiliated with, and, unlike tinder or other apps geared towards meet-ups, allows students to purchase these meals at a discounted price.
+          The discounts are provided by Calvin University Student Senate. The Senate team has access to 10 - 15% discounts on purchases from almost 20 restaurants that are within a 4-mile radius of Calvin University! These restaurants are an integral part of the Calvin University student experience because they provide wonderful study spaces, amazing opportunities for connection, and of course, exquisite 5-star meals.
+          College can be a difficult experience for students to adapt to, and meaningful interactions can be more difficult to come by. It is our belief as the KnightBites team that meals can be an excellent way to fostering discussion and relationships between anyone. Additionally, due to the nature of meet up apps, it can engage people with the culture of Grand Rapids as they enjoy meals.
+      </Text>
+      
+      <Text style ={styles.header}>Team Members</Text>
+      <ol>
+      <li>Aayam Shrestha</li>
+      <li>Anjana Sainju</li>
+      <li>Charles Duimstra</li>
+      <li>Ifeanyi Onyeanakwe</li>
+      </ol>
     </View>
   );
 }
@@ -95,5 +107,9 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontFamily: 'Gotham',
     color: '#F3CD00'
-  }
+  },
+  paragraphs: {
+    padding: 20,
+
+  },  
 });
