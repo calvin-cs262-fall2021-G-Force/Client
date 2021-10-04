5ae7970823f9e5902cbac38081b6263540cbb273
charlesduimstra <charles.duimstra@gmail.com>
2021-10-04 15:44:04 -0400

    modularization of project

---
+++ screens/login.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, View, TouchableOpacity} from 'react-native';
+import { globalStyles } from '../styles/global';
+
+export default function LoginScreen({ navigation }) {
+    /*Create a Login Screen with a button that will ask for Authentication
+      TODO: Will need to have KnightBites logo and other aesthetics added*/
+    return (
+      <View style={globalStyles.screen}>
+        {/*The Button - will eventually take to Calvin's sign in page, but for now just takes you straight to the home page*/}
+        <Text style={globalStyles.header}>
+          Knight Bites
+        </Text>
+        <TouchableOpacity style = {globalStyles.button} onPress= {() => navigation.navigate('Home')}>
+          <Text style = {globalStyles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style = {globalStyles.button} onPress= {() => navigation.navigate('About')}>
+          <Text style = {globalStyles.buttonText}>About Us</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
---
+++ styles/post.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+export const postStyles = StyleSheet.create({
+    item: {
+		backgroundColor: '#FFF',
+        width: '100',
+		padding: 15,
+		borderRadius: 10,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		marginBottom: 20,
+	},
+	itemLeft: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		flexWrap: 'wrap',
+	},
+    /*Square could be replaced by some kind of Profile Picture*/
+	square: {
+		width: 24,
+		height: 24,
+		backgroundColor: '#F3CD00',
+		opacity: 0.4,
+		borderRadius: 5,
+		marginRight: 5,
+	},
+	itemText: {
+		maxWidth: '80%',
+	},
+    /*circle can be replaced with an options menu*/
+    circular: {
+		width: 12,
+		height: 12,
+		borderColor: '#8C2131',
+		borderWidth: 2,
+		borderRadius: 5,
+	},
+});
