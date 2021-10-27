a20bfbb99550e03081528475dc9b33da0283fdf1
iko2 <ifeanyionyeanakwe@gmail.com>
2021-10-27 19:34:19 -0400

    added navbar

--- App.js
+++ App.js
@@ -1,18 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
 import PostScreen from './screens/post'
 import AboutScreen from './screens/about'
 import LoginScreen from './screens/login'
 import HomeScreen from './screens/home'
 import Header from './shared/header'
 
-const Stack = createNativeStackNavigator();
+const TabNavigator = createBottomTabNavigator({
+
+Home: {
+  screen: HomeScreen,
+  navigationOptions: {
+    tabBarLabel: "Home",
+    tabBarOptions: {
+      activeTintColor: "#006600",
+    },
+    tabBarIcon: (tabInfo) => {
+      return (
+        <Ionicons
+          name="md-home"
+          size={24}
+          color={tabInfo.focused ? "#006600" : "#8e8e93"}
+        />
+      );
+    },
+  },
+},
+About: {
+  screen: AboutScreen,
+  navigationOptions: {
+    tabBarLabel: "About",
+    tabBarOptions: {
+      activeTintColor: "#006600",
+    },
+    tabBarIcon: (tabInfo) => {
+      return (
+        <Ionicons
+          name="md-person-circle-outline"
+          size={24}
+          color={tabInfo.focused ? "#006600" : "#8e8e93"}
+        />
+      );
+    },
+  },
+},
+// Profile: {
+//   screen: ProfileScreen,
+//   navigationOptions: {
+//     tabBarLabel: "Profile",
+//     tabBarOptions: {
+//       activeTintColor: "#006600",
+//     },
+//     tabBarIcon: (tabInfo) => {
+//       return (
+//         <Ionicons
+//           name="md-settings-outline"
+//           size={24}
+//           color={tabInfo.focused ? "#006600" : "#8e8e93"}
+//         />
+//       );
+//     },
+//   },
+});
+
+
+const Navigator = createAppContainer(TabNavigator);
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <Navigator>
+      <HomeScreen />
+    </Navigator>
+    /*<NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name='Login' component ={LoginScreen} 
         options={({ navigation }) => ({
@@ -29,6 +91,6 @@ export default function App() {
         <Stack.Screen name='Post' component = {PostScreen} />
         <Stack.Screen name='About' component ={AboutScreen}/>
       </Stack.Navigator>
-    </NavigationContainer>
-  )
+    </NavigationContainer>*/
+  );
 }
