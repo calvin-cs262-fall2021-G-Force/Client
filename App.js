a9ccd946c483c2f0d1d8c681ae286fd7c516aabf
anjanasainju <50439374+anjanasainju@users.noreply.github.com>
2021-11-03 21:40:21 -0400

    Revert "Sign up feature"

--- App.js
+++ App.js
@@ -4,17 +4,16 @@ import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 
-import PostScreen from './screens/post';
-import AboutScreen from './screens/about';
-import LoginScreen from './screens/login';
-import HomeScreen from './screens/home';
+import PostScreen from './screens/post'
+import AboutScreen from './screens/about'
+import LoginScreen from './screens/login'
+import HomeScreen from './screens/home'
 import ProfileScreen from './screens/profile';
-import Header from './shared/header';
-import SignUpScreen from './screens/signup';
-
+import Header from './shared/header'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const TabNavigator = createBottomTabNavigator({
+
 Home: {
   screen: home,
   navigationOptions: {
@@ -92,7 +91,6 @@ function home() {
         })}/>
         <Stack.Screen name='Post' component = {PostScreen} />
         <Stack.Screen name='About the app' component ={AboutScreen}/>
-        <Stack.Screen name='SignUp' component ={SignUpScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
