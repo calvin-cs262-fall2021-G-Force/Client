c8aee98b60a0b05cf58760b0b8e7eee46d5c676e
anjanasainju <anjanasainju@gmail.com>
2021-11-29 21:04:43 -0500

    restaurant screen looks good

--- screens/restaurant.js
+++ screens/restaurant.js
@@ -26,24 +26,45 @@ export default function RestaurantScreen({navigation}) {
 
   return (
     <View style={globalStyles.screen}>
-    <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? <ActivityIndicator /> : (
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <View style= {restaurantStyles.item}>
-            <Text style={restaurantStyles.name}>{item.name}</Text>
-            <Text style={restaurantStyles.details}>Discount: {item.discount}</Text>
-            <Text style={restaurantStyles.details}>Address : {item.address}</Text>
-            {/* <Text style={restaurantStyles.details}>Open hours : {item.openingtime} - {item.closingtime}</Text> */}
-            <Text style={restaurantStyles.details}>Open hours : {moment(item.openingtime).format('h:mm a')} - {item.closingtime}</Text>
-            {/* date={moment(item.posttime).format('MMM Do YYYY, h:mm a')} */}
-            </View>
+      <Text style={restaurantStyles.heading}>Restaurants</Text>
+        <View style={{width:'95%', top:20}}>
+          {isLoading ? <ActivityIndicator /> : (
+            <FlatList
+              data={data}
+              keyExtractor={({ id }, index) => id}
+              renderItem={({ item }) => (
+                <View style= {restaurantStyles.box}>
+                    <Text style={restaurantStyles.name}>{item.name}</Text>
+                    <Text style={restaurantStyles.discount}>Discount: {item.discount}</Text>
+                    <Text style={restaurantStyles.details}>{item.address}</Text>
+                    <Text style={restaurantStyles.details}>Open hours : {moment(item.openingtime,"HH:mm:ss").format('h:mm a')} - {moment(item.closingtime,"HH:mm:ss").format('h:mm a')}</Text>
+                </View>
+              )}
+            />
           )}
-        />
-      )}
+        </View>
     </View>
-    </View>
+    
   );
 };
+{/* <View style={globalStyles.screen}>
+      <View style={globalStyles.postsWrapper}>
+        <Text style={globalStyles.sectionTitle}>Posts</Text>
+        <View style={globalStyles.items}>
+          {isLoading
+            ? <ActivityIndicator />
+            : (
+              <ScrollView >
+              {
+                postItems.map((item, index) => {
+                  return (
+                    <TouchableOpacity style={postStyles.item} key={index} onPress={() => navigation.navigate('Post', {item})}>
+                      <Post title={item.posttitle} date={moment(item.posttime).format('MMM Do YYYY, h:mm a')}/>
+                    </TouchableOpacity>
+                  )
+                })
+              }
+              </ScrollView>
+            )}
+        </View>
+      </View> */}
--- styles/restaurant.js
+++ styles/restaurant.js
@@ -1,23 +1,39 @@
 import { StyleSheet } from 'react-native';
 
 export const restaurantStyles = StyleSheet.create({
-    item: {
+    box: {
 		backgroundColor: '#fff',
-		padding: 10,
+		padding: 20,
 		borderRadius: 8,
 		justifyContent: 'space-between',
-		marginBottom: 20,
-        width:'100%',
+		marginBottom: 15,
+    },
+
+    heading: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        alignItems:'center',
+        color: "#8C2131",
     },
 
     name:{
         fontSize:20,
         fontWeight:'bold',
         color: "#8C2131",
+        marginBottom:10
     },
 
     details:{
         fontSize:14,
-    }
+        marginTop:5
+       // width:'70%',
+    },
+
+    discount:{
+        fontSize:16,
+        marginTop:5,
+        fontWeight:'bold',
+    },
+
 
 });
