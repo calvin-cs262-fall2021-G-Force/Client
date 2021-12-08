5f540129c88ab4e4888344f652179a0d55747ae8
Aayam <aayam2071@gmail.com>
2021-12-08 15:29:33 -0500

    added signup text fields

--- screens/signup.js
+++ screens/signup.js
@@ -15,6 +15,10 @@ import { globalStyles } from "../styles/global";
 export default function SignUpScreen({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [firstname, setFirstName] = useState("");
+  const [lastName, setLastName] = useState(null);
+  const [collegeYear, setCollegeYear] = useState("");
+  const [bio, setBio] = useState("");
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {});
@@ -40,19 +44,74 @@ export default function SignUpScreen({ navigation }) {
     navigation.navigate("Login");
   };
 
+  const createStudent = async () => {
+    await fetch("https://knight-bites.herokuapp.com/students", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        firstname: firstname,
+        lastName: lastName,
+        password: "",
+        collegeyear: collegeYear,
+        bio: bio,
+      }),
+    })
+      .then((responseJson) => {
+        console.log(
+          "createStudent response object:",
+          JSON.stringify(responseJson)
+        );
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <View style={globalStyles.screen}>
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={globalStyles.loginTextBox}
       >
-        <Text>Create an Account</Text>
+        <Text style={globalStyles.signUpText}>Create an Account</Text>
 
         <TextInput
           style={globalStyles.loginInput}
-          placeholder="Username"
+          placeholder="First Name"
+          onChangeText={(text) => setFirstName(text)}
+          value={firstname}
+        />
+
+        <TextInput
+          style={globalStyles.loginInput}
+          placeholder="Last Name"
+          onChangeText={(text) => setLastName(text)}
+          value={lastName}
+        />
+
+        <TextInput
+          style={globalStyles.loginInput}
+          placeholder="Year in College"
+          onChangeText={(text) => setCollegeYear(text)}
+          value={collegeYear}
+        />
+
+        <TextInput
+          style={globalStyles.loginInput}
+          placeholder="Email"
           onChangeText={(text) => setEmail(text)}
           value={email}
+        />
+
+        <TextInput
+          style={globalStyles.loginInput}
+          placeholder="Bio"
+          onChangeText={(text) => setBio(text)}
+          value={bio}
         />
 
         <TextInput
